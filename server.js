4fe3ccbfa5ba47acc28db6e2956adaf49bd9e42d
friendyy <siddharthgupta190@gmail.com>
2022-07-19 22:12:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,35 +3,63 @@ const express = require("express");
 const MongoClient = require('mongodb').MongoClient;; 
 const app = express();
 const dbDetails = require("./database");  //This has connection url and db name only.
+app.set("view engine", "pug");
+// serve static files from the `public` folder
+app.use(express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
 
+//****************** */
 MongoClient.connect(dbDetails.url, function(err, db) {
   if (err) throw err;
   var dbo = db.db(dbDetails.database);
   dbo.collection(dbDetails.collection).find({}).toArray(function(err, result)  {
     if (err) throw err;
-    app.set("view engine", "pug");
+   
+    
 
-// serve static files from the `public` folder
-app.use(express.static(__dirname + "/public"));
+    const person = result.find(p => p.id === req.query.id);
+    res.render("index", {
+      title: "Homepage",
+      people: result
+    });
 
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Homepage",
-    people: result
-  });
-});
 
-app.get("/profile", (req, res) => {
-  const person = result.find(p => p.id === req.query.id);
-  res.render("profile", {
-    title: `About ${person.firstname} ${person.lastname}`,
-    person
-  });
-});
     db.close();
   });
 });
+
+ 
+});
+
+
+app.get("/profile", (req, res) => {
+
+//****************** */
+MongoClient.connect(dbDetails.url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db(dbDetails.database);
+  dbo.collection(dbDetails.collection).find({}).toArray(function(err, result)  {
+    if (err) throw err;
+   
+    
+
+    const person = result.find(p => p.id === req.query.id);
+    res.render("profile", {
+      title: `About ${person.firstname} ${person.lastname}`,
+      person
+    });
+
+
+    db.close();
+  });
+});
+//********************* */
+
+
+});
+
+
 
 
 const server = app.listen(process.env.PORT || 7000, () => {
