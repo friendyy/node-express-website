7aa78c50ecb92576652ec9018be9da68ce66d556
friendyy <siddharthgupta190@gmail.com>
2022-07-20 16:10:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,67 +1,188 @@
-const express = require("express");
-//const people = require("./people.json"); //we are using MongoDB instead of json file
-const MongoClient = require('mongodb').MongoClient;; 
-const app = express();
-const dbDetails = require("./database");  //This has connection url and db name only.
-app.set("view engine", "pug");
-// serve static files from the `public` folder
-app.use(express.static(__dirname + "/public"));
-
-app.get("/", (req, res) => {
-
-//****************** */
-MongoClient.connect(dbDetails.url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db(dbDetails.database);
-  dbo.collection(dbDetails.collection).find({}).toArray(function(err, result)  {
-    if (err) throw err;
-   
-    
-
-    const person = result.find(p => p.id === req.query.id);
-    res.render("index", {
-      title: "Homepage",
-      people: result
-    });
+/* ==========================================================================
+   #STYLES
+   ========================================================================== */
 
 
-    db.close();
-  });
-});
-
- 
-});
-
-
-app.get("/profile", (req, res) => {
-
-//****************** */
-MongoClient.connect(dbDetails.url, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db(dbDetails.database);
-  dbo.collection(dbDetails.collection).find({}).toArray(function(err, result)  {
-    if (err) throw err;
-   
-    
-
-    const person = result.find(p => p.id === req.query.id);
-    res.render("profile", {
-      title: `About ${person.firstname} ${person.lastname}`,
-      person
-    });
+/**
+ *
+ * [1]: Inheriting box sizing slightly better best-practice
+ * https://css-tricks.com/inheriting-box-sizing-probably-slightly-better-best-practice/
+ *
+ * [2]: WordPress System font-stack
+ */
 
 
-    db.close();
-  });
-});
-//********************* */
+/* RESET
+   ========================================================================== */
 
 
-});
+html {
+  box-sizing: border-box;
+}
+
+*, *::before, *::after {
+  box-sizing: inherit; /* [1] */
+  margin: 0;
+  padding: 0;
+}
 
 
 
+/* FONTS
+   ========================================================================== */
 
-const server = app.listen(process.env.PORT || 7000, () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
+body {
+  font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif; /* [2] */
+}
+
+
+
+/* Header
+   ========================================================================== */
+
+.header {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  background-color: #525286;
+  color: #FFFFFF;
+  margin-bottom: 50px;
+}
+
+
+
+/* Homepage Container
+   ========================================================================== */
+
+.container {
+  width: 960px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 20px;
+}
+
+
+
+
+/* Profile Card
+   ========================================================================== */
+
+.person {
+  text-align: center;
+}
+
+.person > * {
+  margin-bottom: 5px;
+}
+
+.person-image {
+  width: 100%;
+  height: 300px;
+}
+
+
+
+/* Profile Page
+   ========================================================================== */
+
+.profile {
+  display: flex;
+  height: 100vh;
+  width: 100%;
+}
+
+.profile-image {
+  height: 100%;
+  flex-grow: 1;
+  flex-basis: 50%;
+}
+
+.profile-details {
+  flex-basis: 30%;
+  flex-grow: 1;
+  padding: 50px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.profile-details > * {
+  margin-bottom: 20px;
+}
+
+
+
+/* Buttons
+   ========================================================================== */
+   .linkbutton {
+    display: block;
+    width: 115px;
+    height: 50px;
+    background: #4E9CAF;
+    padding: 10px;
+    text-align: center;
+    border-radius: 5px;
+    color: white;
+    font-weight: bold;
+    line-height: 25px;
+}
+
+.button {
+  height: 50px;
+  line-height: 50px;
+  width: 100%;
+  text-decoration: none;
+  text-align: center;
+  font-size: 20px
+}
+
+.button-link {
+  color: #FFFFFF;
+  background-color: #659AF1;
+  border-radius: 5px;
+}
+/** forms */
+
+
+
+.form {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	background: white;
+	padding: 1rem;
+	box-shadow: 0px 0px 6px 0px #808080b3;
+	border-radius: 10px;
+}
+
+.form form {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+
+.form form input, .form form textarea {
+	border: 1px solid gray;
+	border-radius: 5px;
+	padding: 5px 10px;
+	font-size: 20px;
+	outline: none;
+}
+
+.form form label {
+	margin-top: 7px;
+}
+
+.form button {
+	margin-top: 10px;
+	padding: 4px 10px;
+	font-size: 1rem;
+	border: 1px solid gray;
+	border-radius: 7px;
+	outline: none;
+	cursor: pointer;
+}
